e84f1cd4c8588920fc2f080fcb60b917cec5c1d9
Roman F <romandesigns.us@gmail.com>
2022-09-07 19:34:44 -0400

    debugging app

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
 
 	const foo = useCallback(() => {
 		const pusher = new Pusher("8ff7fa3c88124600cbf5", {
-			cluster: process.env.CLUSTER,
+			cluster: "us2",
 		});
 		const channel = pusher.subscribe("movie-review");
 		channel.bind("new-review", function (data) {
